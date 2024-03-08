60ac3db9c983d88f6a6c62470cbd04df72cf8a1d
Mohamed Regaya <regaya.mohamed@esprit.tn>
2024-03-08 16:17:11 +0100

    Time: 33 ms (69.29%), Space: 16.5 MB (95.90%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def smallestEvenMultiple(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
         if(1 <= n <= 150):
             if(n%2==0):
                 return n
             else:
                 return n*2
+        
